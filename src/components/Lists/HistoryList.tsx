74658f8ebccac66312ddf5064d51b38e8b383d17
JoseMa <esparev@hotmail.com>
2023-03-29 17:09:45 -0600

    feat: ✨ add refresh control to history list

--- src/components/Lists/HistoryList.tsx
+++ src/components/Lists/HistoryList.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {ScrollView, View, Text, FlatList} from 'react-native';
+import React, {useState, useCallback} from 'react';
+import {ScrollView, View, Text, FlatList, RefreshControl} from 'react-native';
 import HistoryItem from '../Items/HistoryItem';
 import historyItem from '../Items/styles/historyItem';
 import HistoryArrayProps from '../../@types/Lists/HistoryArrayProps';
@@ -7,11 +7,23 @@ import groupHistory from '../../utils/functions/groupHistory';
 import sortByYear from '../../utils/functions/sortByYear';
 
 const HistoryList = (props: HistoryArrayProps): JSX.Element => {
+  const [refreshing, setRefreshing] = useState(false);
+
   const groupedHistory = groupHistory(props);
   const sortedGroupedHistory = sortByYear(groupedHistory);
 
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 1500);
+  }, []);
+
   return (
-    <ScrollView>
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
       {Object.values(sortedGroupedHistory).map(item => (
         <View key={item.year}>
           <Text style={historyItem.year}>{item.year}</Text>
