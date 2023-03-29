4421fa438a3e79fc654a9bd37303115dcb471033
JoseMa <esparev@hotmail.com>
2023-03-29 15:11:13 -0600

    typos: ✏️ change interface name of HistoryArrayProps

--- src/@types/Lists/HistoryArrayProps.ts
+++ src/@types/Lists/HistoryArrayProps.ts
@@ -1,4 +1,13 @@
-interface HistoryItemProps {
+/**
+ * History array props interface for HistoryList component
+ * @interface HistoryArrayProps
+ * @property {object[]} history - History
+ * @property {number} history.id - History id
+ * @property {number} history.month - History month
+ * @property {number} history.year - History year
+ * @property {number} history.total - History total
+ */
+interface HistoryArrayProps {
   history: {
     id: number;
     month: number;
@@ -7,4 +16,4 @@ interface HistoryItemProps {
   }[];
 }
 
-export default HistoryItemProps;
+export default HistoryArrayProps;
