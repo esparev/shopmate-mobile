98d931a162c6b172b941c5b41d50cd4418418c17
JoseMa <esparev@hotmail.com>
2023-03-30 01:06:27 -0600

    construction: 🚧 add mock for stores

---
+++ src/mocks/stores.js
@@ -0,0 +1,14 @@
+const stores = [
+  {id: 1, name: 'Walmart', location: 'Mexico City'},
+  {id: 2, name: 'Chedraui', location: 'Guadalajara'},
+  {id: 3, name: 'Soriana', location: 'Monterrey'},
+  {id: 4, name: 'HEB', location: 'San Antonio'},
+  {id: 5, name: 'OXXO', location: 'Mexico City'},
+  {id: 6, name: 'Superama', location: 'Guadalajara'},
+  {id: 7, name: 'Walmart', location: 'Monterrey'},
+  {id: 8, name: 'Chedraui', location: 'San Antonio'},
+  {id: 9, name: 'HEB', location: 'Mexico City'},
+  {id: 10, name: 'OXXO', location: 'Guadalajara'},
+];
+
+export default stores;
